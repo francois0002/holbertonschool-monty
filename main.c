a783345616adc68d1736efbdafb3fb9201aaface
francois0002 <francois.gosselin.lille@gmail.com>
2024-01-03 16:07:47 +0100

    Commit add Mhamed new author + main init

--- main.c
+++ main.c
@@ -11,6 +11,24 @@
 
 int main(int argc, char *argv[])
 {
-printf("Hello World\n");
+     if (argc != 2) {
+        fprintf(stderr, "USAGE: %s file\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    FILE *file = fopen(argv[1], "r");
+
+    if (!file)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    stack_t *stack;
+
+    process_file(file);
+
+    fclose(file);
+
 return (0);
 }
