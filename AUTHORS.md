7fdb3089920bf64e12b2789d4e58b53b5e3093c6
francois0002 <francois.gosselin.lille@gmail.com>
2024-01-03 11:07:05 +0100

    First commit . Add file structure, Read me and authors

---
+++ AUTHORS.md
@@ -0,0 +1,7 @@
+<div align="center"> 
+  <h3>AUTHORS</h3>
+</div>
+
+- Alexis Billemont : alexis.billemont@holbertonstudents.com
+
+- François Gosselin : gosselin.francois@holbertonstudents.com
---
+++ main.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+printf("Hello World\n");
+return (0);
+}
